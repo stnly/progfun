0b52b77f1082546bd3a9c65455ca2eeec2fbc6d1
Stanley Tan <me@stnly.com>
2013-03-27 09:51:47 +1100

    Implement max method

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -41,5 +41,8 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    if (xs.isEmpty) throw new java.util.NoSuchElementException()
+    else xs.reduceLeft(_ max _)
+  }
 }
