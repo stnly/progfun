15124db6c1de472631d8b25b3824c734f2b7014a
Stanley Tan <me@stnly.com>
2013-04-05 22:48:24 +1100

    Implement intersect method

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -32,7 +32,7 @@ object FunSets {
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = ???
+  def intersect(s: Set, t: Set): Set = {elem => s(elem) && t(elem)}
 
   /**
    * Returns the difference of the two given sets,
