04bd05035c9e0579f6f1caa425868b4df4aff9b4
Stanley Tan <me@stnly.com>
2013-04-17 18:12:55 +1000

    Implement singleton test

--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -38,6 +38,13 @@ class HuffmanSuite extends FunSuite {
     assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
   }
 
+  test("singleton") {
+    new TestTrees {
+      assert(singleton(List(t1)))
+      assert(!singleton(List(t1, t2)))
+    }
+  }
+
   test("combine of some leaf list") {
     val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
     assert(combine(leaflist) === List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
