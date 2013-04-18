4de013e8645096f1917bbb2ede7331439cc7eb10
Stanley Tan <me@stnly.com>
2013-04-18 17:16:58 +1000

    Implement quickEncode

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -242,5 +242,6 @@ object Huffman {
    * To speed up the encoding process, it first converts the code tree to a code table
    * and then uses it to perform the actual encoding.
    */
-  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+  def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] =
+    text.flatMap(codeBits(convert(tree)))
 }
